5a996b2be1f1f85c98eb9dcad81b7e76139f1846
mustafafaqiry <faqirym9@gmail.com>
2022-01-18 10:21:03 +0500

    creating a simple birthday reminder with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { data } from "./data";
 
-function App() {
+const App = () => {
+  const removeitems = () => {
+    setstate([]);
+  };
+  const [state, setstate] = useState(data);
+  const persons = state.length;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <section className="container">
+          <h1>{persons} birthdays today</h1>
+          {state.map((person) => {
+            const { id, name, age, image } = person;
+            return (
+              <article className="person" key={id}>
+                <img src={image} alt={name} />
+                <div key={id}>
+                  <h3>{name}</h3>
+                  <p>{age} Years</p>
+                </div>
+              </article>
+            );
+          })}
+          <button className="btn" onClick={removeitems}>
+            Clear All
+          </button>
+        </section>
+      </main>
+    </>
   );
-}
+};
 
 export default App;
